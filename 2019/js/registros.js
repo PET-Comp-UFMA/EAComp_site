622ad634fdd60c8b46287cf00be720abf8b45913
ArthurPassos16 <arthurovitalio@gmail.com>
2021-08-24 14:42:19 -0300

    finalização da pagina de registros

--- 2019/js/registros.js
+++ 2019/js/registros.js
@@ -2,14 +2,20 @@ var dia1 = document.getElementById("dia1");
 var dia2 = document.getElementById("dia2");
 var dia3 = document.getElementById("dia3");
 
+var item_dia1 = document.getElementById('item-dia1')
+var item_dia2 = document.getElementById('item-dia2')
+var item_dia3 = document.getElementById('item-dia3')
+
 function exibeDiaUm() {
     if(dia1.className === "collapse") {
         dia1.className = "collapse show";
         dia2.className = "collapse";
         dia3.className = "collapse";
-    } else {
-        dia1.className = "collapse";
-    }
+
+        item_dia1.className = "col-sm-4 text-lg-center active"
+        item_dia2.className = "col-sm-4 text-lg-center"
+        item_dia3.className = "col-sm-4 text-lg-center"
+    } 
 }
 
 function exibeDiaDois() {
@@ -17,8 +23,10 @@ function exibeDiaDois() {
         dia2.className = "collapse show";
         dia1.className = "collapse";
         dia3.className = "collapse";
-    } else {
-        dia2.className = "collapse";
+        
+        item_dia1.className = "col-sm-4 text-lg-center"
+        item_dia2.className = "col-sm-4 text-lg-center active"
+        item_dia3.className = "col-sm-4 text-lg-center"
     }
 }
 
@@ -27,7 +35,9 @@ function exibeDiaTres() {
         dia3.className = "collapse show";
         dia2.className = "collapse";
         dia1.className = "collapse";
-    } else {
-        dia3.className = "collapse";
+        
+        item_dia1.className = "col-sm-4 text-lg-center"
+        item_dia2.className = "col-sm-4 text-lg-center"
+        item_dia3.className = "col-sm-4 text-lg-center active"
     }
 }
