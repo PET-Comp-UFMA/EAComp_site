b0f7ed8a75ad2c74b83277e94480816747118eba
IA-V <iagoviper48@gmail.com>
2021-08-24 12:01:53 -0300

    Correção dos dropdowns do menu na página de registros - EAComp 2019

--- 2019/js/registros.js
+++ 2019/js/registros.js
@@ -3,31 +3,31 @@ var dia2 = document.getElementById("dia2");
 var dia3 = document.getElementById("dia3");
 
 function exibeDiaUm() {
-    if(dia1.className === "collapse") {
-        dia1.className = "collapse show";
-        dia2.className = "collapse";
-        dia3.className = "collapse";
+    if(dia1.style.display === "none") {
+        dia1.style.display = "block";
+        dia2.style.display = "none";
+        dia3.style.display = "none";
     } else {
-        dia1.className = "collapse";
+        dia1.style.display = "none";
     }
 }
 
 function exibeDiaDois() {
-    if(dia2.className === "collapse") {
-        dia2.className = "collapse show";
-        dia1.className = "collapse";
-        dia3.className = "collapse";
+    if(dia2.style.display === "none") {
+        dia2.style.display = "block";
+        dia1.style.display = "none";
+        dia3.style.display = "none";
     } else {
-        dia2.className = "collapse";
+        dia2.style.display = "none";
     }
 }
 
 function exibeDiaTres() {
-    if(dia3.className === "collapse") {
-        dia3.className = "collapse show";
-        dia2.className = "collapse";
-        dia1.className = "collapse";
+    if(dia3.style.display === "none") {
+        dia3.style.display = "block";
+        dia2.style.display = "none";
+        dia1.style.display = "none";
     } else {
-        dia3.className = "collapse";
+        dia3.style.display = "none";
     }
 }
