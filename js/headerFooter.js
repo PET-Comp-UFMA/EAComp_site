6cf75dddfe5b9f148fabae2d2c20aadce06716e5
JuanCarlosM1 <64020519+JuanCarlosM1@users.noreply.github.com>
2024-06-18 11:51:33 -0300

    Corrigido Header
    Header agora destaca a página atual

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -1,7 +1,28 @@
+function destacarBotao(){
+  let nomeArquivo = window.location.pathname.split('/').pop();
+  let botao;
+  if(nomeArquivo=="index.html"){
+    botao = document.getElementById("headerHome");
+  }else if(nomeArquivo=="trabalhos.html" || nomeArquivo=="videos-depoimentos.html"){
+    botao = document.getElementById("headerChamada");
+  }else if(nomeArquivo=="programacao.html"){
+    botao = document.getElementById("headerProgramação");
+  }else if(nomeArquivo=="inscricoes.html"){
+    botao = document.getElementById("headerInscrição");
+  }else if(nomeArquivo=="formularioTutorias.html" || nomeArquivo=="local.html" || nomeArquivo=="comissao.html"){
+    botao = document.getElementById("HeaderInformação");
+  }
+
+  if(botao!=undefined){
+    botao.classList.add("menu-active");
+  }
+}
+
 fetch('header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-component').innerHTML = data;
+    destacarBotao();
   });
 
 fetch('footer.html')
