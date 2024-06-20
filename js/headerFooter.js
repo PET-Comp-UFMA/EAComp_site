0cb334d2e7d22ecb036832c18d62c3f2c68a751c
JuanCarlosM1 <64020519+JuanCarlosM1@users.noreply.github.com>
2024-06-20 10:04:05 -0300

    Atualizado destaque do elementos do header
    Agora o header destaca apenas o elemento em que o mouse está em cima removendo o destaque da página atual.

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -1,7 +1,7 @@
 function destacarBotao(){
   let nomeArquivo = window.location.pathname.split('/').pop();
   let botao;
-  
+
   if(nomeArquivo=="trabalhos.html" || nomeArquivo=="videos-depoimentos.html"){
     botao = document.getElementById("headerChamada");
   }else if(nomeArquivo=="programacao.html"){
@@ -24,6 +24,27 @@ fetch('header.html')
   .then(data => {
     document.getElementById('header-component').innerHTML = data;
     destacarBotao();
+    const header = document.getElementById('nav-menu');
+
+    header.addEventListener('mouseover', function() {
+      console.log('O mouse está sobre a div.');
+      let teste = document.getElementsByClassName('menu-active');
+
+      for(let i=0;i<teste.length;i++){
+        teste[i].classList.remove('menu-active')
+        console.log(teste[i]);
+      }
+      
+      header.addEventListener('mouseleave', function(){
+        destacarBotao();
+        header.removeEventListener('mouseleave', function() {
+          // Seu código aqui
+        });
+      })
+
+    });
+    
+    
   });
 
 fetch('footer.html')
